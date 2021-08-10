d23681d34582e5a070709248d3ec16088a4b2236
Emad-Eldeen <emadhusameldeen@gmail.com>
2021-08-10 19:49:18 +0200

    initial package project

---
+++ android/src/main/java/ca/elev8xr/flutter_native_view_example/FlutterNativeViewExamplePlugin.java
@@ -0,0 +1,38 @@
+package ca.elev8xr.flutter_native_view_example;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+/** FlutterNativeViewExamplePlugin */
+public class FlutterNativeViewExamplePlugin implements FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_native_view_example");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+}
---
+++ example/android/app/src/main/java/ca/elev8xr/flutter_native_view_example_example/MainActivity.java
@@ -0,0 +1,6 @@
+package ca.elev8xr.flutter_native_view_example_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
